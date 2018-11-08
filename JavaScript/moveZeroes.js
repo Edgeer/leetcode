ff6128dd44b81cfb00f16b967afb4e00d6f1e095
Edgeer <35493913+Edgeer@users.noreply.github.com>
2018-11-08 14:03:16 +0800

    add moveZeroes

---
+++ JavaScript/moveZeroes.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    var i,j=0,length = nums.length;
+    for (i = 0;i<length;){
+        if(nums[i] == 0){
+            nums.splice(i,1);
+            nums.push(0);
+            length--;
+        }else{
+            i++;
+        }
+    }
+
+    return nums;
+};
+console.log(moveZeroes([0,1,0,3,12]));

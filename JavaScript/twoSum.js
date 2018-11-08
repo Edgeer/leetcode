e31061570bb0160562c72ad173b5f1b83e4a715e
Edgeer <35493913+Edgeer@users.noreply.github.com>
2018-11-08 14:32:28 +0800

    两数之和

--- JavaScript/twoSum.js
+++ JavaScript/twoSum.js
@@ -1,1 +1,24 @@
-
+/**
+ * @param  {number[]} nums
+ * @param  {number} target
+ */
+var twoSum = function(nums, target) {
+    var result=[];
+    for(var i=0;i<nums.length-1;i++){
+        for(var j=i+1;j<nums.length;j++){
+            if(nums[i]+nums[j]==target){
+                result.push(i);
+                result.push(j);
+                return result;
+            }
+        }   
+    }
+    return false;
+};
+var getTime = function(func, nums, target) {
+    let start = new Date();
+    console.log(func(nums, target));
+    let end = new Date();
+    console.log('运行时间: ' + (end - start));
+}
+console.log(getTime(twoSum,[2, 3, 5, 5], 8));
